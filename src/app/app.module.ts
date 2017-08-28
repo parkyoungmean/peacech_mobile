c06529af21a08af0f4434fa02d171fc70640962b
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2017-08-28 19:08:03 +0900

    wordpress 연동작업

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WpProvider } from '../providers/wp/wp';
 
+import { Http, HttpModule } from '@angular/http';
+import {
+  WpApiModule,
+  WpApiLoader,
+  WpApiStaticLoader
+} from 'wp-api-angular';
+import { ConstantProvider } from '../providers/constant/constant';
+
+
+export function WpApiLoaderFactory(http) {
+  return new WpApiStaticLoader(http, 'http://peacech.net/wp-json/');
+}
+ 
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +28,12 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    WpApiModule.forRoot({
+      provide: WpApiLoader,
+      useFactory: (WpApiLoaderFactory),
+      deps: [Http]
+    }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +44,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    WpProvider,
+    ConstantProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ToastController, ToastOptions, NavController, LoadingController, Loading } from 'ionic-angular';
+import { WpProvider, Post, OldPost } from '../../providers/wp/wp';
+import { Observable } from 'rxjs/Observable';
+
+//나
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  loader: Loading;
+  posts: Observable<Post[]>;
+  oldPosts: Observable<OldPost[]>;  
+  toastOptions: ToastOptions;
+  message: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private toast:ToastController, public http: Http, public navCtrl: NavController, public wpProvider: WpProvider, public loadingCtrl: LoadingController) {
+    this.presentLoading();
+    this.posts = this.wpProvider.getPosts();
+    this.posts.subscribe(data => { 
+        console.log('my data:', data);
+        this.loader.dismiss();
+    });
+    
+    this.oldPosts = this.wpProvider.getOldtPosts();
+    this.oldPosts.subscribe(data => { 
+      console.log('my data:', data);
+      this.loader.dismiss();
+  });
+    
 
+    
+
+
+
+    this.toastOptions = {
+      message: '평화교회에 오신 걸 환영합니다',
+      duration: 5000,
+      //position: "top"
+    }
+    this.showToast();
+  }
+ 
+  getUserImage(id: number) {
+    return this.wpProvider.getUserImage(id);
+  }
+ 
+  getUserName(id: number) {
+    return this.wpProvider.getUserName(id);
+  }
+ 
+  openPost(post: Post) {
+    this.navCtrl.push('PostPage', {post: post});
+  }
+ 
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Loading..."
+    });
+    this.loader.present();
   }
 
+  showToast(){
+    const toast = this.toast.create(this.toastOptions);
+    toast.present();
+  }
 }
